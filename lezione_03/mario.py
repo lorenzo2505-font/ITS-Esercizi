461dca759d357514c572fb76f67cabf4ae6c9dfe
lorenzo2505-font <acomannilorenzo@gmail.com>
2025-02-18 10:04:38 +0100

    it's me mario

---
+++ lezione_03/mario.py
@@ -0,0 +1,12 @@
+posizione = int(input("inserisci posizione: "))
+if posizione == 1:
+    print("1st!")
+if posizione == 2:
+    print("2st!")
+if posizione == 3:
+    print("3st!")
+if posizione == 4:
+    print("4st!")
+else:
+    print(f"{posizione}st!")
+    
