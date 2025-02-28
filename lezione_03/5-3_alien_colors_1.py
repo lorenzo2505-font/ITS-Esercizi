cc9b4e116c7102dc98657b4e1434159b40cb453a
lorenzo2505-font <acomannilorenzo@gmail.com>
2025-02-28 19:20:11 +0100

    beccati sto commit

---
+++ lezione_03/5-3_alien_colors_1.py
@@ -0,0 +1,24 @@
+'''5-3. Colori alieni n. 1: immagina che un alieno sia stato appena abbattuto durante un gioco. Crea una variabile chiamata alien_color e assegnale 
+il valore "verde", "giallo" o "rosso".
+• Scrivi un'istruzione if per verificare se il colore dell'alieno è verde. Se lo è, stampa un messaggio che il giocatore ha appena guadagnato 5 punti.
+• Scrivere una versione di questo programma che superi il test if e un'altra che fallisce. (La versione che fallisce non avrà output.)'''
+
+#sucesso
+'''
+alien_color: str = "verde"
+
+if alien_color == "verde":
+    print("hai guadagnato 5 punti")
+else:
+    pass'''
+
+#fallimento
+
+alien_color: str = "rosso"
+
+if alien_color == "verde":
+    print("hai guadagnato 5 punti")
+else:
+    pass
+
+
---
+++ lezione_03/5-4_Colori_alieni_n_2.py
@@ -0,0 +1,23 @@
+'''5-4. Colori alieni n. 2: scegli un colore per un alieno come hai fatto nell'esercizio 5-3 e scrivi una catena if-else.
+• Se il colore dell'alieno è verde, stampa una dichiarazione che il giocatore ha appena guadagnato 5 punti per aver sparato all'alieno.
+• Se il colore dell'alieno non è verde, stampa una dichiarazione che il giocatore ha appena guadagnato 10 punti.
+• Scrivere una versione di questo programma che esegua il blocco if e un'altra che esegua il blocco else.'''
+
+#blocco if
+
+'''
+alien_color: str = "verde"
+
+if alien_color == "verde":
+    print("hai, perso l'alieno, hai guadagnato 5 punti")
+else:
+    print("hai appena guadgnato 10 punti")'''
+
+
+#blocco else
+alien_color: str = "rosso"
+
+if alien_color == "verde":
+    print("hai, perso l'alieno, hai guadagnato 5 punti")
+else:
+    print("hai appena guadgnato 10 punti")
