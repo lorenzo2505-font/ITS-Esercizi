58b33ade47f186f5b91ed883abab04ba1e8010b5
lorenzo2505-font <acomannilorenzo@gmail.com>
2025-06-17 21:41:49 +0200

    com

--- lezione_14/my_project/tests/test_calculator.py
+++ lezione_14/my_project/tests/test_calculator.py
@@ -5,3 +5,21 @@ def test_addition():
 
     calculation: Calculator = Calculator(10, 5)
     assert calculation.addition() == 13, "the sum is wrong"
+
+
+def test_substraction():
+
+    calculation: Calculator = Calculator(10, 5)
+    assert calculation.subtraction() == 5, "the substraction is wrong"
+
+
+def test_multiplication():
+
+    calculation: Calculator = Calculator(10, 5)
+    assert calculation.multiplication() == 50, "the multiplication is wrong"
+
+
+def test_division():
+
+    calculation: Calculator = Calculator(10, 5)
+    assert calculation.division() == 2.00, "the quotient is wrong"
