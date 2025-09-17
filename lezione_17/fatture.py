b2396dd6abf59f6938016721c6ba3fd5664d15b3
lorenzo2505-font <acomannilorenzo@gmail.com>
2025-09-18 00:16:32 +0200

    com

--- lezione_17/fatture.py
+++ lezione_17/fatture.py
@@ -7,4 +7,31 @@ class Fattura:
 
         if dottore.isAValidDoctor():
             self.fatture: int = len(pazienti)
+            self.salary: int = 0
+            self.pazienti = pazienti
+            self.dottore = dottore
+        
+        else:
+            self.pazienti = None
+            self.dottore = None
+            self.fatture = None
+            self.fatture = None
+
+            raise Exception('Non è possibile creare la classe fattura poichè il dottore non è valido!')
+    
+    def getSalary(self) -> float:
+        self.salary = self.dottore.getParcel() * len(self.pazienti)
+
+        return self.salary
+    
+    def getFatture(self) -> int:
+        return self.fatture
+    
+    def addPatient(self, newPatient: Paziente):
+
+        if newPatient in self.pazienti:
+            raise ValueError("il paziente è gia presente nell'elenco")
+        
+        self.pazienti.append(newPatient)
+        
         
