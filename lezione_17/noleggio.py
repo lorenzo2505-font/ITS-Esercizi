22973d2999bcb314c930281036eb326aa014dfd6
lorenzo2505-font <acomannilorenzo@gmail.com>
2025-09-19 13:59:19 +0200

    pass

--- lezione_17/noleggio.py
+++ lezione_17/noleggio.py
@@ -1,1 +1,22 @@
 from generi import *
+
+class Noleggi:
+    def __init__(self, film_list: list[Film]): # film_list sono i contenuti all'interno del negozio, rented_films sono i film noleggiati
+        self.film_list = film_list
+        self.rented_film: dict[int, list[Film]]
+    
+    def isAvaible(self, film: Film):
+
+        if film in self.film_list:
+            print(f"Il film scelto è disponibile: {film.getTitle()}!")
+
+            return True
+        
+        print(f"Il film scelto non è disponibile: {film.getTitle()}!")
+
+        return False
+    
+    def rentAMovie(self, film: Film, clientID: int):
+        pass
+
+
